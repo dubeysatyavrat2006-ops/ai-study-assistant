26843427ebea27c07ce7ec198a7f00785a5acc31
Satyavrat Dubey <dubey.satyavrat2006@gmail.com>
2026-05-02 16:56:02 +0530

    Added deployment config

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+import path from "path";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.post("/api/ai", async (req, res) => {
   const { type, text } = req.body;
@@ -54,6 +54,8 @@ app.post("/api/ai", async (req, res) => {
     res.json({ result: "❌ Error connecting to AI" });
   }
 });
+
+app.use(express.static("."));
 
 // ✅ CORRECT POSITION
 app.listen(PORT, () =>
